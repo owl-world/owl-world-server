0c2b188273caf34d97377158ea0a50c8032fb38f
kys6879 <kys6879@naver.com>
2023-02-25 18:40:47 +0900

    리뷰 등록 안되는점 수정

--- src/main/java/com/owls/owlworld/review/ReviewController.java
+++ src/main/java/com/owls/owlworld/review/ReviewController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +30,7 @@ public class ReviewController {
     }
 
     @PostMapping("")
-    public List<Integer> addReview(AddReviewRequest addReviewRequest, HttpServletRequest request) {
+    public List<Integer> addReview(@RequestBody AddReviewRequest addReviewRequest, HttpServletRequest request) {
         Long memberId = Long.valueOf(String.valueOf(request.getAttribute("memberId")));
 
         return reviewService.addReview(addReviewRequest, memberId);
